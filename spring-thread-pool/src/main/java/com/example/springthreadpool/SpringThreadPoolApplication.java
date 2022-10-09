c040db3ac16220f3931fd1ca55738aa1d6243fd1
yhnoh <tedgom19@gmail.com>
2022-10-09 15:06:50 +0900

    learn thread pool

---
+++ spring-thread-pool/src/main/java/com/example/springthreadpool/SpringThreadPoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.springthreadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThreadPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThreadPoolApplication.class, args);
+    }
+
+}
---
+++ spring-thread-pool/src/test/java/com/example/springthreadpool/SpringThreadPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springthreadpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThreadPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
