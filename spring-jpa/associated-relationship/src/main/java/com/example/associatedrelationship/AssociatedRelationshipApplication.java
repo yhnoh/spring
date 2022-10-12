7cc39d9d4364c0833d13e0775c19a43297c426cb
yhnoh <tedgom19@gmail.com>
2022-10-12 22:27:39 +0900

    learn spring jpa associated-relationship

---
+++ spring-jpa/associated-relationship/src/main/java/com/example/associatedrelationship/AssociatedRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.associatedrelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociatedRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssociatedRelationshipApplication.class, args);
+    }
+
+}
