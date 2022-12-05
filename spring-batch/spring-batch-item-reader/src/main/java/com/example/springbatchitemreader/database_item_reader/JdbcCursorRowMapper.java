ef1fe3f28221226f5836f1c643ebc515869df9fb
yhnoh <tedgom19@gmail.com>
2022-12-05 23:38:23 +0900

    jdbc itemReader 알아보기

---
+++ spring-batch/spring-batch-item-reader/src/main/java/com/example/springbatchitemreader/database_item_reader/JdbcCursorRowMapper.java
@@ -0,0 +1,25 @@
+package com.example.springbatchitemreader.database_item_reader;
+
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcCursorRowMapper implements RowMapper<Customer> {
+    @Override
+    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Customer customer = new Customer();
+        customer.setId(rs.getLong("id"));
+        customer.setAddress(rs.getString("address"));
+        customer.setCity(rs.getString("city"));
+        customer.setFirstName(rs.getString("firstName"));
+        customer.setLastName(rs.getString("lastName"));
+        customer.setMiddleInitial(rs.getString("middleInitial"));
+        customer.setState(rs.getString("state"));
+        customer.setZipCode(rs.getString("zipCode"));
+
+        return customer;
+    }
+}
