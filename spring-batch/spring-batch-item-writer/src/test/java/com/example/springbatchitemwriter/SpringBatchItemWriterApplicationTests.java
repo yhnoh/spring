183401d6b5ab4446a04f91368605a29c27ff0348
yhnoh <tedgom19@gmail.com>
2022-12-14 22:35:04 +0900

    spring batch itemWriter 프로젝트 셋팅

---
+++ spring-batch/spring-batch-item-writer/src/test/java/com/example/springbatchitemwriter/SpringBatchItemWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchitemwriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchItemWriterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
