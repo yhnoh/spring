bd88157f3da26e994927f5280e1f6b156c6b4773
yhnoh <tedgom19@gmail.com>
2023-02-08 22:45:46 +0900

    testcontainer 생명주기 파악

--- spring-test-container/src/test/java/com/example/springtestcontainer/lifecycle/singleton/AbstractSingletonContainerTest.java
+++ spring-test-container/src/test/java/com/example/springtestcontainer/lifecycle/singleton/AbstractSingletonContainerTest.java
@@ -1,14 +1,10 @@
 package com.example.springtestcontainer.lifecycle.singleton;
 
 import org.testcontainers.containers.MariaDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-//@Testcontainers
+//https://www.testcontainers.org/test_framework_integration/junit_5/
 public class AbstractSingletonContainerTest {
 
-//    @Container
-//    static final MariaDBContainer MARIA_DB_CONTAINER = new MariaDBContainer("mariadb:10.3.8");
 
     //여러 테스트 클래스에 대해 한 번만 시작되는 컨테이너를 정의하는 것이 유용
     //Testcontainers 확장에서 제공하는 이 사용 사례에 대한 특별한 지원은 없습니다. 대신 다음 패턴을 사용하여 구현할 수 있습니다.
