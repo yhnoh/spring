5d0745000b8b84cca7fb9d38cc89a366314a5583
yhnoh <tedgom19@gmail.com>
2023-03-07 22:33:39 +0900

    spring msa gateway 알아보기

---
+++ spring-msa/spring-msa-gateway/src/main/java/com/example/springmsagateway/SpringMsaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmsagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsaGatewayApplication.class, args);
+    }
+
+}
