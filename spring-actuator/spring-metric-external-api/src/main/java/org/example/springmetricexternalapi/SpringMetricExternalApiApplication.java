1115d39b287fedc1693b3f675234310875739241
yhnoh <tedgom19@gmail.com>
2024-08-28 22:08:38 +0900

    프로젝트 셋팅

---
+++ spring-actuator/spring-metric-external-api/src/main/java/org/example/springmetricexternalapi/SpringMetricExternalApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.springmetricexternalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMetricExternalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMetricExternalApiApplication.class, args);
+    }
+
+}
---
+++ spring-actuator/spring-metric-external-api/src/test/java/org/example/springmetricexternalapi/SpringMetricExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springmetricexternalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMetricExternalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
