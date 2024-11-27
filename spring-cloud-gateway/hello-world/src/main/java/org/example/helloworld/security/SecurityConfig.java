ee1cbb283a63b2b580829739dca92ee9a538af05
yhnoh <tedgom19@gmail.com>
2024-11-27 22:45:28 +0900

    spring cloud gateway 대해서 알아보기

---
+++ spring-cloud-gateway/hello-world/src/main/java/org/example/helloworld/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.example.helloworld.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user").password("password").roles("USER").build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+
+    }
+}
